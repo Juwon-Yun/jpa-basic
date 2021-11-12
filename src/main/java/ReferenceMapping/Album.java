1b948721196c7b376d2e8a4e1195cf2747dca47b
Juwon-Yun <eooll2@daum.net>
2021-11-12 16:56:01 +0900

    ReferenceMapping

--- src/main/java/ReferenceMapping/Album.java
+++ src/main/java/ReferenceMapping/Album.java
@@ -1,8 +1,14 @@
 package ReferenceMapping;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+/*
+	@DiscriminatorValue("NAME")으로 DTYPE에 들어갈 데이터의 이름을 정해줄 수 있다.
+
+ */
 @Entity
+@DiscriminatorValue("A")
 public class Album  extends  Item{
 
 	private String artist;
--- src/main/java/ReferenceMapping/Item.java
+++ src/main/java/ReferenceMapping/Item.java
@@ -5,6 +5,12 @@ import javax.persistence.*;
 /*
 	별도의 설정을 하지않고 extends를 하면 단일 테이블전략을 사용한다
 	@DiscriminatorColumn을 지정하면 DTYPE 컬럼이 생긴다
+	   => (name = "@@@") 으로 하면 @@@ 컬럼 이름으로 생김
+	@Inheritance(strategy = InheritanceType.JOINED) => 상속 관계 테이블 전략
+	@Inheritance(strategy = InheritanceType.SINGLE_TABLE) => 단일 테이블 전략
+	@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) => 구현 클래스마다 테이블 전략
+		=> (Item 테이블을 abstract(추상클래스)로 설정해야한다)
+
  */
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
